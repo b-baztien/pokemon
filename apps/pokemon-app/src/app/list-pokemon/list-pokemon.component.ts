153e063279558c174dc953f8ff2dacbc147c31d9
Poomin Yennattee <devilbastopoftheworld@gmail.com>
2021-10-15 12:27:37 +0700

    search pokemon

--- apps/pokemon-app/src/app/list-pokemon/list-pokemon.component.ts
+++ apps/pokemon-app/src/app/list-pokemon/list-pokemon.component.ts
@@ -1,11 +1,37 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { from, fromEvent, Observable } from 'rxjs';
 import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  defer,
+  EMPTY,
+  from,
+  fromEvent,
+  iif,
+  interval,
+  merge,
+  Observable,
+  Subject,
+  Subscription,
+} from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import {
+  catchError,
+  concatAll,
+  concatMap,
   debounceTime,
   map,
-  mergeAll,
+  mergeMap,
+  skip,
+  skipWhile,
+  startWith,
   switchMap,
+  switchMapTo,
+  takeWhile,
+  tap,
   toArray,
 } from 'rxjs/operators';
 import { Pokemon } from '../interfaces/pokemon';
@@ -17,41 +43,91 @@ import { PokemonDetail } from '../interfaces/pokemon-detail';
   templateUrl: './list-pokemon.component.html',
   styleUrls: ['./list-pokemon.component.css'],
 })
-export class ListPokemonComponent implements OnInit {
+export class ListPokemonComponent implements OnInit, OnDestroy {
   @ViewChild('inputText', { static: true, read: ElementRef })
   inputText!: ElementRef<HTMLInputElement>;
 
   offset = 0;
-  limit? : number;
-  listPokemon$!: Observable<PokemonDetail[]>;
-  pokemonEvent$!: Observable<Event>;
+  limit: number = 100;
+  pokemonName: string = '';
+  listPokemonSub$!: Subscription;
+  listPokemon: PokemonDetail[] = [];
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.pokemonEvent$ = fromEvent(this.inputText!.nativeElement, 'input');
-
-    this.listPokemon$ = this.pokemonEvent$.pipe(
-      debounceTime(400),
-      switchMap(() => this.searchPokemon()),
-      switchMap((res) =>
-        from(res.map((item) => this.getPokemonDetail(item))).pipe(
-          mergeAll(),
-          toArray()
-        )
-      )
-    );
-  }
-
-  searchPokemon() {
-    return this.http
-      .get<PokemonCount>(
+  fetchPokemon$ = defer(() => {
+    console.log(`offset=${this.offset}&limit=${this.limit}`);
+    return ajax
+      .getJSON<PokemonCount>(
         `https://pokeapi.co/api/v2/pokemon/?offset=${this.offset}&limit=${this.limit}`
       )
-      .pipe(map((response) => response.results));
+      .pipe(
+        map((res) => {
+          if (this.limit >= res.count) throw new Error(`Is All Pokemon`);
+          return res.results;
+        }),
+        catchError((err) => {
+          console.error(err);
+          return EMPTY;
+        })
+      );
+  });
+
+  getPokemonDetail$ = (pokemon: Pokemon) =>
+    ajax.getJSON<PokemonDetail>(pokemon.url).pipe(
+      catchError((err) => {
+        console.error(err);
+        return EMPTY;
+      })
+    );
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const pokemonEvent$ = fromEvent(
+      this.inputText!.nativeElement,
+      'input'
+    ).pipe(
+      debounceTime(400),
+      tap(() => {
+        console.log(this.pokemonName);
+      })
+    );
+
+    const pokemon$ = new Subject();
+
+    this.listPokemonSub$ = pokemonEvent$
+      .pipe(
+        switchMap(() => {
+          this.listPokemon = [];
+          this.offset = 0;
+          
+          return pokemon$.pipe(startWith(null));
+        }),
+        switchMap(() => this.fetchPokemon$),
+        concatMap((res) => {
+          return from(res.map((item) => this.getPokemonDetail$(item))).pipe(
+            concatAll(),
+            toArray(),
+            map((res) => {
+              const listFilterPokemon = res.filter((pokemon) =>
+                pokemon.name.startsWith(this.pokemonName)
+              );
+              return (this.listPokemon = [
+                ...this.listPokemon,
+                ...listFilterPokemon,
+              ]);
+            })
+          );
+        }),
+        takeWhile((res) => res.length < 20),
+        tap(() => {
+          this.offset += this.limit;
+          pokemon$.next();
+        })
+      )
+      .subscribe();
   }
 
-  getPokemonDetail(pokemon: Pokemon) {
-    return this.http.get<PokemonDetail>(pokemon.url);
+  ngOnDestroy(): void {
+    this.listPokemonSub$.unsubscribe();
   }
 }
